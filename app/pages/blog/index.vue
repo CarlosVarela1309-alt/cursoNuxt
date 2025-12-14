badfa53a573437b6f6fd9b3d0cfcd6bd045bb617
CarlosVarela1309-alt <charlyvarela771@gmail.com>
2025-12-14 16:40:13 -0600

    feat: update blog page to fetch and display posts from API

--- app/pages/blog/index.vue
+++ app/pages/blog/index.vue
@@ -1,5 +1,10 @@
 <template>
     <div>
         <h1>Blog Page</h1>
+        <pre>{{ JSON.stringify(posts, null, 2) }}</pre>
     </div>
 </template>
+
+<script setup lang="ts">
+    const {data: posts} = await useFetch("https://jsonplaceholder.typicode.com/posts" );
+</script>
