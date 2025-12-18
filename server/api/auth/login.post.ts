c069739d977616004fd294b34d5423214f9d5bbb
CarlosVarela1309-alt <charlyvarela771@gmail.com>
2025-12-18 12:51:14 -0600

    feat: enhance login functionality with session management and error handling

--- server/api/auth/login.post.ts
+++ server/api/auth/login.post.ts
@@ -1,12 +1,26 @@
-import { z } from 'zod'
+import { z } from 'zod';
 
 const bodySchema = z.object({
     email: z.email().trim().toLowerCase(),
     password: z.string().min(6),
-})
-
-export default eventHandler(async (event) => {
-    const {email, password} = await readValidatedBody(event, bodySchema.parse)
-    return { email, password };
 });
 
+export default eventHandler(async (event) => {
+    const { email, password } = await readValidatedBody(event, bodySchema.parse);
+
+    if (email === "test@test.com" && password === "123123") {
+        await setUserSession(event, {
+            user: {
+                email: email,
+                username: "testuser",
+            }
+        })
+
+        return {};
+    }
+
+    throw createError({
+        statusCode: 401,
+        statusMessage: "Invalid email or password",
+    })
+});
